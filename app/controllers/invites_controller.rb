42e7e23d7a8242f020a49a1b3327dd88b33b72ce
uwadonat <uwadonat@gmail.com>
2021-04-09 13:38:32 -0400

    update invitation in invite model

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -14,9 +14,26 @@ class InvitesController < ApplicationController
     end
   end
 
+  def edit
+    @invite = Event.find(params[:id]).invites.where(user_name: current_user.name)
+  end
+  #  @new = Invite.where(event_id: event.id).where(user_name: current_user.name) 
+  def update
+    @invite = Event.find(params[:id]).invites.where(user_name: current_user.name)
+    @invite.attendence = true
+    @invite.update(update_params)
+      flash[:success] = 'invitation updated'
+
+  end
+
+
   private
 
   def invite_params
     params.require(:invite).permit(:user_name)
+  end
+
+  def update_params
+    params.require(:invite).permit(:user_name, :attendence)
   end
 end
--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,4 +1,4 @@
 class Invite < ApplicationRecord  
   belongs_to :event
-
+  attr_writer :attendence
 end
